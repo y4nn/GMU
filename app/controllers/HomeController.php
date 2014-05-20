ed5c181d8211e4a88971f3d08bba064e5906b8f7
y4nn <y4nn.maillard@gmail.com>
2014-05-20 18:37:44 +0200

    Merge origin/master
    Conflicts:
    	.gitignore
    	nbproject/project.properties
    	nbproject/project.xml

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,42 +1,36 @@
 <?php
 
 class HomeController extends BaseController {
-    
+
     protected $layout = "layout.main";
-    protected $user;
-    
-    public function homeController(){    
-    $user = new User();
+
+    public function homeController() {
+        
     }
-    
-    
 
     /*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
+      |--------------------------------------------------------------------------
+      | Default Home Controller
+      |--------------------------------------------------------------------------
+      |
+      | You may wish to use controllers instead of, or in addition to, Closure
+      | based routes. That's great! Here is an example controller method to
+      | get you started. To route to this controller, just add the route:
+      |
+      |	Route::get('/', 'HomeController@showWelcome');
+      |
+     */
 
-	public function showWelcome()
-	{
-            return View::make('hello');
-	}
-        
-        public function getUsers() {
-            
-            $user = User::all();
-            var_dump($user[0]->prenom);
+    public function showWelcome() {
+        return View::make('hello');
+    }
 
-            $this->layout->content = View::make('users')->with('user', $user);
-        }
-        
-        
+    public function getUsers() {
+
+        $user = User::all();
+        var_dump($user[0]->prenom);
+
+        $this->layout->content = View::make('users')->with('user', $user);
+    }
 
 }
